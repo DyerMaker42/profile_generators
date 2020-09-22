c293f557a8019895221df5322418ec86602806fc
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-22 22:05:37 +0000

    added russian doll logic

--- survey.js
+++ survey.js
@@ -5,8 +5,61 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js?', (answer) => {
-  console.log(`Thank you for your valuble feedback: ${answer}`);
+let answers = {};
 
-  rl.close();
+rl.question(`What's your name? Nicknames are also acceptable :)`, (answer) => {
+  anwswers[nickname] = answer;
+  
+  rl.question(`What's an activity you like doing?`, (answer) => {
+  
+    answers[activity] = answer;
+  
+    rl.question(`What do you listen to while doing that?`, (answer) => {
+  
+      answers[listen] = answer;
+    
+      rl.question(`Which meal is your favourite (eg: dinner, brunch, etc.)`, (answer) => {
+  
+        answers[meal] = answer;
+      
+        rl.question(`What's your favourite thing to eat for that meal?`, (answer) => {
+  
+          answers[food] = answer;
+        
+          rl.question(`Which sport is your absolute favourite?`, (answer) => {
+  
+            answers[sport] = answer;
+          
+           
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+          });
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+        });
+          
+        
+      });
+      
+    });
+    
+  });
+
 });
+
